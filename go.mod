4d1056250e7b79a39617f45706ea2315fb051e73
cy <yuxia5611@gmail.com>
2025-05-14 16:20:22 +0800

    feat:change log

--- go.mod
+++ go.mod
@@ -2,13 +2,15 @@ module sdk-go-example-test
 
 go 1.24.0
 
-require github.com/scrapeless-ai/scrapeless-actor-sdk-go v0.2.5
+require github.com/scrapeless-ai/scrapeless-actor-sdk-go v0.2.6
 
 require (
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
@@ -26,6 +28,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/redis/go-redis/v9 v9.7.3 // indirect
 	github.com/rs/zerolog v1.34.0 // indirect
 	github.com/sagikazarmark/locafero v0.7.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 	defer actor.Close()
 	var param = &RequestParam{}
 	if err := actor.Input(param); err != nil {
-		log.GetLogger().Error().Msg(err.Error())
+		log.Error(err.Error())
 		return
 	}
 	test([]byte(`{"name":"test"}`))
@@ -39,40 +39,40 @@ func main() {
 			Payload: []byte(fmt.Sprintf(`{"name":"%s-%d"}`, param.Name, i)),
 		})
 		if err != nil {
-			log.GetLogger().Error().Msg(err.Error())
+			log.Error(err.Error())
 		}
-		log.GetLogger().Info().Msgf("push msgId:%s\n", msgId)
+		log.Infof("push msgId:%s", msgId)
 		pullResp, err := actor.Storage.GetQueue().Pull(context.Background(), 1)
 		if err != nil {
-			log.GetLogger().Error().Msg(err.Error())
+			log.Error(err.Error())
 		}
-		log.GetLogger().Info().Msgf("get msgId %s,msg:%+v\n", pullResp[0].ID, pullResp)
+		log.Infof("get msgId %s,msg:%+v", pullResp[0].ID, pullResp)
 		err = actor.Storage.GetQueue().Ack(context.Background(), msgId)
 		if err != nil {
-			log.GetLogger().Error().Msg(err.Error())
+			log.Error(err.Error())
 		}
-		log.GetLogger().Info().Msgf("ack msgId:%s\n", msgId)
+		log.Infof("ack msgId:%s", msgId)
 	}
 	actor.Server.AddHandle("/test", test)
 	data, err := actor.Router.Request(param.Keyword, http.MethodGet, "/hello", nil, nil)
 	if err != nil {
-		log.GetLogger().Error().Msg(err.Error())
+		log.Error(err.Error())
 	}
-	log.GetLogger().Info().Msgf("get data:%s\n", string(data))
+	log.Infof("get data:%s", string(data))
 	go func() {
 		if err := actor.Start(); err != nil {
-			log.GetLogger().Error().Msg(err.Error())
+			log.Error(err.Error())
 		}
-		log.GetLogger().Info().Msgf("actor start success at port : %s", env.Env.Actor.HttpPort)
+		log.Infof("actor start success at port : %s", env.Env.Actor.HttpPort)
 	}()
 	time.Sleep(time.Second * 10)
 	request, err := actor.Router.Request(env.Env.Actor.RunId, http.MethodPost, "/test", strings.NewReader(`{"name":"cy"}`), map[string]string{
 		"Content-Type": "application/json",
 	})
 	if err != nil {
-		log.GetLogger().Error().Msg(err.Error())
+		log.Error(err.Error())
 	}
-	log.GetLogger().Info().Msgf("get data:%s\n", string(request))
+	log.Infof("get data:%s", string(request))
 	for {
 		randLog()
 		time.Sleep(time.Second)
@@ -97,19 +97,19 @@ func test(t []byte) (httpserver.Response, error) {
 				"time": time.Now().Unix(),
 			},
 		}
-		log.GetLogger().Info().Msgf("add items: %+v\n", items)
+		log.Infof("add items: %+v", items)
 		_, err := actor.Storage.GetDataset().AddItems(context.Background(), items)
 		if err != nil {
-			log.GetLogger().Error().Msg(err.Error())
+			log.Error(err.Error())
 		}
 		getItems, err := actor.Storage.GetDataset().GetItems(context.Background(), 1, 10, false)
 		if err != nil {
-			log.GetLogger().Error().Msg(err.Error())
+			log.Error(err.Error())
 		}
-		log.GetLogger().Info().Msgf("get items: %+v\n", getItems)
+		log.Infof("get items: %+v", getItems)
 		time.Sleep(time.Second)
 	}
-	log.GetLogger().Info().Msg("add items success")
+	log.Info("add items success")
 
 	for i := 0; i < 2; i++ {
 		_, err := actor.Storage.GetKv().SetValue(
@@ -118,16 +118,16 @@ func test(t []byte) (httpserver.Response, error) {
 			fmt.Sprintf("value-%d", i),
 			0)
 		if err != nil {
-			log.GetLogger().Error().Msgf("set value err:%v\n", err)
+			log.Errorf("set value err:%v", err)
 		}
 		value, err := actor.Storage.GetKv().GetValue(context.Background(), fmt.Sprintf("key-%d", i))
 		if err != nil {
-			log.GetLogger().Error().Msgf("get value err:%v\n", err)
+			log.Errorf("get value err:%v", err)
 		}
-		log.GetLogger().Info().Msgf("get value: %s\n", value)
+		log.Infof("get value: %s", value)
 		time.Sleep(time.Second)
 	}
-	log.GetLogger().Info().Msg("add items success !")
+	log.Info("add items success !")
 	//put, err := actor.Storage.GetObject().Put(context.Background(), "key.json", []byte(fmt.Sprintf(`{"name":"%s"}`, param.Name)))
 	//if err != nil {
 	//	panic(err)
@@ -150,21 +150,21 @@ func randLog() {
 	switch logLevel {
 	case 0:
 		// 模拟输出Trace级别日志
-		log.GetLogger().Trace().Msg("This is a trace log")
+		log.Trace("This is a trace log")
 	case 1:
 		// 模拟输出Debug级别日志
-		log.GetLogger().Debug().Msg("This is a debug log")
+		log.Debug("This is a debug log")
 	case 2:
 		// 模拟输出Info级别日志
-		log.GetLogger().Info().Msg("This is an info log")
+		log.Info("This is an info log")
 	case 3:
 		// 模拟输出Warn级别日志
-		log.GetLogger().Warn().Msg("This is a warning log")
+		log.Warn("This is a warning log")
 	case 4:
 		// 模拟输出Error级别日志
-		log.GetLogger().Error().Msg("This is an error log")
+		log.Error("This is an error log")
 	default:
 		// 模拟输出Info级别日志
-		log.GetLogger().Info().Msg("This is an info log")
+		log.Info("This is an info log")
 	}
 }
